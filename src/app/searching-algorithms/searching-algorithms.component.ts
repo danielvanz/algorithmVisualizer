c6ce0bf29d07d7c6f110278e995047aeba19963a
Daniël <d.a.v.ziel@student.tue.nl>
2023-01-16 20:33:58 +0100

    Added target selection

--- src/app/searching-algorithms/searching-algorithms.component.ts
+++ src/app/searching-algorithms/searching-algorithms.component.ts
@@ -9,10 +9,8 @@ import { Coordinate } from '../chess-board';
 export class SearchingAlgorithmsComponent {
 
     horseTile: string = "../assets/knight.png";
-    horseTileIndex: Coordinate = {
-      x: -1,
-      y: -1
-    }
+    horseTileIndex: Coordinate = {x: -1, y: -1};
+    targetTileCoordinate: Coordinate = {x: -1, y: -1};
 
     buttonImages : string[] = [
       "../assets/king_white.png", 
@@ -34,10 +32,56 @@ export class SearchingAlgorithmsComponent {
     [0,0,0,0,0,0,0,0]]
 
 
+  gameStarted: boolean = false;
 
-  pressedOnTile(i :number, j :number) {
-      this.horseTileIndex.x = j
-      this.horseTileIndex.y = i
+  startGame() {
+    if (this.horseTileIndex.x == -1 || this.targetTileCoordinate.x == -1) return; //Implement error handling
+    this.gameStarted = true;
+    this.chessBoard[this.horseTileIndex.y][this.horseTileIndex.x] = 1;
+    this.breathFirstSearch();
+  }
+  
+  resetGame() {
+    this.gameStarted = false;
+    this.horseTileIndex.x = -1;
+    this.horseTileIndex.y = -1;
+    this.targetTileCoordinate.x = -1;
+    this.targetTileCoordinate.y = -1;
+    this.chessBoard = [
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]]
   }
 
+  pressedOnTile(i :number, j :number) {
+    if (this.gameStarted) return;
+
+    if (this.horseTileIndex.x == -1) {
+      this.chessBoard = [
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]]
+      this.chessBoard[i][j] = 1  
+      this.horseTileIndex.x = j;
+      this.horseTileIndex.y = i;
+    } else if (this.targetTileCoordinate.x == -1) {
+      this.targetTileCoordinate.x = j;
+      this.targetTileCoordinate.y = i;
+      this.chessBoard[i][j] = 2;
+    }
+    }
+
+    breathFirstSearch() {
+    }
 }
+
